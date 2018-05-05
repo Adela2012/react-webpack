05d5930adfcd50e3265028df332664f4f60de9c7
Danying Yang <15757124551@163.com>
2018-05-05 23:51:35 +0800

    home

---
+++ app/components/HomeHeader/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import PureRenderMixin from 'react-addons-pure-render-mixin'
+import './style.less'
+
+class HomeHeader extends React.Component {
+    constructor (props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
+    render() {
+        return (
+            <div className="home-header">
+                <header className="index-head">
+                    <a className="city">杭州</a>
+                    <div className="search">输入商户名、地点</div>
+                    <a className="self">
+                        <div className="selfOutLine"></div>
+                    </a>
+                </header>
+                <div className="header-fill"></div>
+            </div>
+        )
+    }
+}
+
+export default HomeHeader
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 var path = require('path')
 var webpack = require('webpack')
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var OpenBrowserPlugin = require('open-browser-webpack-plugin');
 
 module.exports = {
     entry: path.resolve(__dirname, 'app/index.jsx'),
     output: {
+        path: __dirname + "/build",
         filename: "bundle.js"
     },
 
@@ -19,8 +21,12 @@ module.exports = {
             { test: /\.less$/, exclude: /node_modules/, loader: 'style!css!postcss!less' },
             { test: /\.css$/, exclude: /node_modules/, loader: 'style!css!postcss' },
             { test: /\.(png|gif|jpg|jpeg|bmp)$/i, loader: 'url-loader?limit=5000' },  // 限制大小5kb
-            { test: /\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader: 'url-loader?limit=5000' } // 限制大小小于5k
+            { test: /\.(woff|woff2|svg|ttf|eot)($|\?)/i, loader: 'url-loader?limit=5000' } // 限制大小小于5k
         ]
+    },
+
+    eslint: {
+        configFile: '.eslintrc'
     },
 
     postcss: [
@@ -48,7 +54,16 @@ module.exports = {
     ],
 
     devServer: {
+        proxy: {
+            // 凡是 `/api` 开头的 http 请求，都会被代理到 localhost:3000 上，由 koa 提供 mock 数据。
+            // koa 代码在 ./mock 目录中，启动命令为 npm run mock
+            '/api': {
+                target: 'http://localhost:3000',
+                secure: false
+            }
+        },
         port: 8023,
+        contentBase: "./public", //本地服务器所加载的页面所在的目录
         colors: true, //终端中输出结果为彩色
         historyApiFallback: true, //不跳转，在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
         inline: true, //实时刷新
