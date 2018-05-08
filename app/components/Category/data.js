7be2b78c6ac2754fde7486b4d8c51d57bbea1c13
Danying Yang <15757124551@163.com>
2018-05-08 08:39:05 +0800

    category

--- app/components/Category/data.js
+++ app/components/Category/data.js
@@ -52,105 +52,105 @@
     ], 
     [
         {
-            img: 'home_01.png',
-            title: '美食',
+            img: 'home_11.png',
+            title: '婚纱摄影',
             link: '/'
         },
         {
-            img: 'home_02.png',
-            title: '猫眼电影',
+            img: 'home_12.png',
+            title: '生活服务',
             link: '/'
         },
         {
-            img: 'home_03.png',
-            title: '酒店',
+            img: 'home_13.png',
+            title: '景点',
             link: '/'
         },
         {
-            img: 'home_04.png',
-            title: '休闲娱乐',
+            img: 'home_14.png',
+            title: '爱车',
             link: '/'
         },
         {
-            img: 'home_05.png',
-            title: '外卖',
+            img: 'home_15.png',
+            title: '运动健身',
             link: '/'
         },
         {
-            img: 'home_06.png',
-            title: '火锅',
+            img: 'home_16.png',
+            title: '亲子',
             link: '/'
         },
         {
-            img: 'home_07.png',
-            title: '丽人',
+            img: 'home_17.png',
+            title: '家装',
             link: '/'
         },
         {
-            img: 'home_08.png',
-            title: '购物',
+            img: 'home_18.png',
+            title: '学习培训',
             link: '/'
         },
         {
-            img: 'home_09.png',
-            title: '周边游',
+            img: 'home_19.png',
+            title: '医疗健康',
             link: '/'
         },
         {
-            img: 'home_10.png',
-            title: 'KTV',
+            img: 'home_20.png',
+            title: '到家',
             link: '/'
         }
     ], 
     [
         {
-            img: 'home_01.png',
-            title: '美食',
+            img: 'home_21.png',
+            title: '小吃快餐',
             link: '/'
         },
         {
-            img: 'home_02.png',
-            title: '猫眼电影',
+            img: 'home_22.png',
+            title: '自助餐',
             link: '/'
         },
         {
-            img: 'home_03.png',
-            title: '酒店',
+            img: 'home_23.png',
+            title: '日本料理',
             link: '/'
         },
         {
-            img: 'home_04.png',
-            title: '休闲娱乐',
+            img: 'home_24.png',
+            title: '美发',
             link: '/'
         },
         {
-            img: 'home_05.png',
-            title: '外卖',
+            img: 'home_25.png',
+            title: '美甲美睫',
             link: '/'
         },
         {
-            img: 'home_06.png',
-            title: '火锅',
+            img: 'home_26.png',
+            title: '美容SPA',
             link: '/'
         },
         {
-            img: 'home_07.png',
-            title: '丽人',
+            img: 'home_27.png',
+            title: '瘦身纤体',
             link: '/'
         },
         {
-            img: 'home_08.png',
-            title: '购物',
+            img: 'home_28.png',
+            title: '亲子摄影',
             link: '/'
         },
         {
-            img: 'home_09.png',
-            title: '周边游',
+            img: 'home_29.png',
+            title: '亲子游乐',
             link: '/'
         },
         {
-            img: 'home_10.png',
-            title: 'KTV',
+            img: 'home_30.png',
+            title: '全部分类',
             link: '/'
         }
     ]
